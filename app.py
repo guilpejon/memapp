1de7451106316e825030dc314d3191458b8d3555
Guilherme <guilpejon@gmail.com>
2020-11-15 22:02:40 -0300

    Finished reddit scraper

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import praw
 import sys
+import os
+import pprint
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Load the local source directly
 sys.path.insert(1, "./python-slack-sdk")
@@ -9,13 +17,19 @@ logging.basicConfig(level=logging.DEBUG)
 
 from slack_sdk import WebClient
 
-slack_token = "xoxb-1501480777732-1519169041296-qNTdPCnS8UmOLIpKugODwTdM"
+slack_token = os.environ.get("SLACK_TOKEN")
 client = WebClient(token=slack_token)
 
-try:
+reddit = praw.Reddit(
+    client_id=os.environ.get("REDDIT_APP_ID"),
+    client_secret=os.environ.get("REDDIT_APP_SECRET"),
+    user_agent='MemeApp'
+)
+subreddit = reddit.subreddit("ProgrammerHumor")
+
+for submission in subreddit.top(limit=1):
+    pprint.pprint(vars(submission))
     api_response = client.chat_postMessage(
         channel="random",
-        text="Hello from your app! :tada:"
+        text=submission.url
     )
-except SlackApiError as e:
-    assert e.response["error"]
